5c1fe5f27b8d2aac2bfef2a8b169210b3019d109
Cristofaro <cri.cecoro06@gmail.com>
2026-07-07 09:54:33 +0200

    update docker for frontend and backend for routes

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+// WELCOME PAGE
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Basics
+
+// Route::prefix('v1')->group(function () {
+
+//     Route::post('/register', [AuthController::class, 'register']);
+//     Route::post('/login', [AuthController::class, 'login']);
+
+// });
